2c6defa8c9216737af78a844f8ce93743c9575bd
Anatoly Dryga <anatolydryga@gmail.com>
2015-10-06 16:47:06 -0400

    uniform errors in reads

--- sequencing_error.R
+++ sequencing_error.R
@@ -1,3 +1,5 @@
+library(stringr)
+
 #' introduce sequencing error in reads
 #' @param miseq_reads dataframe with 4 columns: R1, R2, I1, qname character vectors
 #' @param seq_error sequencing error specification
@@ -17,5 +19,49 @@ generate_seq_error <- function(miseq_reads, seq_error) {
 #' @return character vector of sequences with errors
 uniform_seq_error <- function(sequences, seq_error) {
     stopifnot(seq_error >= 0 & seq_error <= 100)
-    sequences
+    n_errors <- get_number_errors(sequences, seq_error)
+    mapply(change_nucleotides, sequences, n_errors, USE.NAMES=FALSE)
+}
+
+#' how many errors need to be introduced
+#' @param sequences character vector of error-free seq
+#' @param seq_error percentage of nucleotide to change
+#' @return integer vector of number of erros to introduce for each seq
+get_number_errors <- function(sequences, seq_error) {
+    n_sequences <- length(sequences)
+    seq_len <- sapply(sequences, nchar, USE.NAMES=FALSE)
+    n_errors <- (seq_len*seq_error)/100.00 # we can have 1.4 bases to modify
+    n_errors_int <- floor(n_errors) # that takes care of int part
+    n_errors_reminder <- n_errors -  n_errors_int
+    add_one <- runif(n_sequences) < n_errors_reminder # that takes care of reminder
+    n_errors_int + add_one # average is close to expected error rate
+}
+
+#' substitute nucleotides
+#' @param sequence characters of DNA representation
+#' @param n_error number of nucleotide to change
+change_nucleotides <- function(sequence, n_error) {
+    positions <- sample(1:nchar(sequence), n_error)
+    sequence_errors <- sequence
+    null <- sapply(positions, function(pos) {
+        sequence_errors <<- change_nucleotide_at(sequence_errors, pos)
+    })
+    sequence_errors
+}
+
+UNAMBIGUOUS_DNA_ALPHABET <- c('A', 'G', 'T', 'C')
+
+#' change one nucleotide in sequence
+#' @param sequence characters of DNA representation
+#' @param position at what position to introduce error
+change_nucleotide_at <- function(sequence, position) {
+    stopifnot(position <= nchar(sequence))
+    prefix <- str_sub(sequence, start=1, end=position-1)
+    nucleotide_to_change <- str_sub(sequence, start=position, end=position)
+    suffix <- str_sub(sequence, start=position+1)
+
+    neighbours <- setdiff(UNAMBIGUOUS_DNA_ALPHABET, nucleotide_to_change)
+    erroneous_nucleotide <- sample(neighbours, 1)
+
+    str_c(prefix, erroneous_nucleotide, suffix)
 }
--- test_sequencing_error.R
+++ test_sequencing_error.R
@@ -29,15 +29,16 @@ test_that("seq of 10 bases and 10% error lead to 1 error in each read", {
 })
 
 large_seqs <- c(seqs, seqs, seqs, "AAAAAAAAAA") # 10 sequences total
+large_seqs <- rep(large_seqs, 10) # finally 100 sequences
 
 test_that("seq of 10 bases and 15% error: number of errors differ ", {
-    seq_error <- uniform_seq_error(seqs, seq_error=15)
-    # so should have around 15 errors
-    n_errors <- stringdist(seqs, seq_error, method="hamming")
+    seq_error <- uniform_seq_error(large_seqs, seq_error=15)
+    # so should have around 100 sequences * 10 basepairs * 15 % error = 150
+    n_errors <- stringdist(large_seqs, seq_error, method="hamming")
     expect_true(all(n_errors >= 1), 'each read have at least one error')
     expect_true(any(n_errors >= 2), 'some reads have 2 errors')
     n_errors_total <- sum(n_errors)
-    # 15 +- 3
-    expect_true(n_errors_total > 12)
-    expect_true(n_errors_total < 18)
+    # 150 +- 15
+    expect_true(n_errors_total > 135)
+    expect_true(n_errors_total < 165)
 })
